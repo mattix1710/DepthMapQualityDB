e8af73744bbde27d8f33328dcc329d14bf3ae67a
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2022-09-22 14:37:23 +0200

    added REGEX title checker

--- depth_grader/depthQualifier/models.py
+++ depth_grader/depthQualifier/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .validators import *
 from django.core.exceptions import ValidationError
+import re
 
 # Create your models here.
 def seq_location(instance, seq_name):
@@ -18,8 +19,14 @@ def validate_project_name_exist(value):
         if(elem.title.lower() == value.lower()):
             raise ValidationError('Project of the same name already exists!')
 
+def validate_project_name_correct(value):
+    if not re.search("^[a-zA-Z_][a-zA-Z_ ]{4,}$", value):
+        if not re.search("^.{5,}$", value):
+            raise ValidationError('Title should have at least 5 characters!')
+        raise ValidationError('Title encloses invalid characters!\nIt should include only a-z, A-Z, _ and space characters.')
+
 class SequenceModel(models.Model):
-    title = models.CharField(max_length=30, unique=True, validators=[validate_project_name_exist])
+    title = models.CharField(max_length=30, unique=True, validators=[validate_project_name_exist, validate_project_name_correct])
     desc = models.TextField()
     src = models.FileField(upload_to=seq_location, validators=[validate_archive_extension])
 
