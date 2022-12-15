e63b881707ec3ed2ebcfcd4ea10417eddaede854
Wojciech Kaczmarek <wojtak123@gmail.com>
2022-12-15 18:29:54 +0100

    Wojciech 15.12 CSS Update

--- depth_grader/depthQualifier/forms.py
+++ depth_grader/depthQualifier/forms.py
@@ -9,6 +9,11 @@ class UploadZipForm(forms.ModelForm):
     # # description - textarea
     # desc = forms.CharField(widget=forms.Textarea(attrs={'rows':5, 'cols':20}))
 
+    error_css_class = 'error-field'
+    required_css_class = 'required_field'
+    title = forms.CharField(widget=forms.TextInput(attrs={"class": "form-title", "placeholder": "Sequence title"}))
+    desc = forms.CharField(widget=forms.Textarea(attrs={"class": "form-desc", "placeholder": "Sequence description"}))
+
     class Meta:
         # def __init__(self, *args, **kwargs):
         #     super(UploadZipForm, self).__init__(*args, **kwargs)
@@ -19,7 +24,7 @@ class UploadZipForm(forms.ModelForm):
         labels = {
             'title': 'Nazwa sekwencji',
             'desc': 'Opis sekwencji',
-            'src': 'Archiwum ZIP z sekwencją',
+            'src': 'Sequence ZIP archive',
         }
 
         model = SequenceModel
