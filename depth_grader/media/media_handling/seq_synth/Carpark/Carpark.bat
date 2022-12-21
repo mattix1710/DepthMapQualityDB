d66dcdb6d98c8ecd10f158f70c443e1561631b19
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2022-12-21 19:38:29 +0100

    Updated synth config files

---
+++ depth_grader/media/media_handling/seq_synth/Carpark/Carpark.bat
@@ -0,0 +1,3 @@
+.\MParallel.exe --count=10 .\TAppVvs.exe -c Carpark_VVS_0.cfg : .\TAppVvs.exe -c Carpark_VVS_1.cfg : .\TAppVvs.exe -c Carpark_VVS_2.cfg : .\TAppVvs.exe -c Carpark_VVS_3.cfg : .\TAppVvs.exe -c Carpark_VVS_4.cfg : .\TAppVvs.exe -c Carpark_VVS_5.cfg : .\TAppVvs.exe -c Carpark_VVS_6.cfg : .\TAppVvs.exe -c Carpark_VVS_7.cfg : .\TAppVvs.exe -c Carpark_VVS_8.cfg : .\TAppVvs.exe -c Carpark_VVS_9.cfg
+
+EXIT
