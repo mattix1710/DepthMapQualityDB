ca546949673282c0d2bb9317a1b573ec815bc2e2
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-01-06 20:31:26 +0100

    Added initial data (sequences)

--- depth_grader/depthQualifier/admin.py
+++ depth_grader/depthQualifier/admin.py
@@ -3,5 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(MethodProposal)
-admin.site.register(Sequence)
 admin.site.register(SeqDepthResult)
+admin.site.register(Sequence)
