24f49a7fa026d87a8e35e563f765c276c84884e5
Mateusz Szuda <64504618+mattix1710@users.noreply.github.com>
2023-01-09 19:56:06 +0100

    Update tasks.py

--- depth_grader/depthQualifier/tasks.py
+++ depth_grader/depthQualifier/tasks.py
@@ -5,24 +5,11 @@ from celery import shared_task
 from .models import MethodProposal
 from .src.functions import mul_zip_unpack, mul_batch_synthesis, mul_process_data
 
-# TODO: TO DELETE
-# MATEUSZ
-# @shared_task
-# def process_the_sequence(seq_id):          # arguments in this function need to be serializable (i.e. string, int, etc.)
-#     seq = SequenceModel.objects.get(id=seq_id)
-#     print("SRC:", seq.src)
-    # zipUnpack(str(seq.src))
-    # batchSynthesis(seq)
-    # processPSNR(seq, str(seq.src))
-    
-##############################################
-# multicolumn
-
 # MATEUSZ
 @shared_task
-def process_the_depth_method(method_id):
+def process_the_depth_method(method_id):    # arguments in this function need to be serializable (i.e. string, int, etc.)
     depth_method = MethodProposal.objects.get(method_id=method_id)
-    # mul_zip_unpack(str(depth_method.src))          # probably DONE
-    # mul_batch_synthesis(depth_method)              # probably DONE
+    mul_zip_unpack(str(depth_method.src))          # probably DONE
+    mul_batch_synthesis(depth_method)              # probably DONE
     # processing PSNR and bitrate data METHOD
-    # mul_process_data(depth_method, str(depth_method.src))
+    mul_process_data(depth_method, str(depth_method.src))
